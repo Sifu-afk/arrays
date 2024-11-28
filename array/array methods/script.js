e36e73f6d7914c667650b2797177a341bd815a5b
Gytis <123653687+Sifu-afk@users.noreply.github.com>
2024-11-28 14:05:35 +0200

    Add files via upload

---
+++ array/array methods/script.js
@@ -0,0 +1,63 @@
+const uno = ['vienas', 'du', 'trys']
+const dos = ['keturi', 'penki', 'sesi']
+const number = [1, 2, 3, 4, 5, 6]
+////
+//console.log(uno.concat(dos))
+////
+//console.log(uno.every(isNaN))
+////
+//const ran = ['number']
+//console.log(ran.fill(2))
+////
+//console.log(dos.filter(isNaN))
+////
+//const bigNumber = (element) => element > 2;
+//console.log(number.find(bigNumber))
+////
+//const findNumber = (element) => element > 1
+//console.log(number.findIndex(findNumber))
+////
+//uno.forEach((element) => console.log(element));
+////
+//console.log(number.includes(1))
+////
+//console.log(number.indexOf(2))
+////
+//console.log(uno.join(dos))
+////
+//console.log(number.lastIndexOf(1))
+////
+//console.log(uno.length)
+////
+//const map1 = number.map((x) => x *2)
+//console.log(map1)
+////
+//console.log(number.pop())
+//console.log(number)
+////
+//console.log(number.push(6))
+//console.log(number)
+////
+//const myReducer = (returnValue, currentValue) =>
+ //   returnValue + currentValue * currentValue;
+//console.log(number.reduce(myReducer))
+////
+//console.log(uno.reverse())
+////
+//console.log(number.shift())
+//console.log(number)
+////
+//console.log(uno.slice(1))
+////
+//const even = (element) => element % 2 ===0
+//console.log(number.some(even))
+////
+//console.log(uno.sort())
+////
+//console.log(uno.splice(1))
+////
+//console.log(uno.unshift('nulis'))
+//console.log(uno)
+
+
+
